7c447b7b3be25750b9a4333f0f8288ac6c812535
amz2694 <alimiree.zade@gmail.com>
2022-06-06 19:35:33 +0430

    verifyJWT middleware

---
+++ back/middleware/verifyJWT.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader) return res.sendStatus(401);
+    if (!authHeader.startsWith('Bearer ')) return res.sendStatus(401);
+    const token = authHeader.split(' ')[1];
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.sendStatus(403); //invalid token
+            req.email = decoded.userInfo.email;
+            next();
+        }
+    );
+}
+
+module.exports = verifyJWT;
