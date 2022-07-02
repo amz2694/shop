9026cbcc46f9a02781d0a45856d4e2c3d1c6b4d9
amz2694 <alimiree.zade@gmail.com>
2022-07-02 11:36:37 +0430

    header

---
+++ front/src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+      <div class="header-container">
+        <div class="cart-container">
+            <img src="../assets/cart.png" alt="cart" class="cartimg">
+            <span>500</span>
+        </div>
+        <div class="logo-container">
+            <span>SHOP</span>
+        </div>
+        <div class="option-container">
+            <input type="search" class="search" placeholder="search">
+            <span class="register">register</span>
+            <span class="login">login</span>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src='../style/header.css'>
+
+</style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
+  <Header/>
   <div class="home">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Header from '../components/header.vue'
 
 export default {
   name: 'Home',
-  components: {}
+  components: {Header}
 }
 </script>
