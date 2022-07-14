5d7374eda06fb24f01c742d21946cea8c2d3538c
amz2694 <alimiree.zade@gmail.com>
2022-07-14 18:08:19 +0430

    comment section

---
+++ front/src/components/comment.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="comment-container">
+        <p class="date">{{ item.date }}</p>
+        <p class="text">{{ item.text }}</p>
+        <p class="replay">replay</p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'comment',
+    props : ['item'],
+}
+</script>
+
+<style src="../style/comment.css">
+
+</style>
--- front/src/views/product.vue
+++ front/src/views/product.vue
@@ -1,64 +1,75 @@
 <template>
   <div class="productpage-container" v-if="!show404">
-      <img :src="img" class="productimg">
-      <div class="productdetails-container">
-          <p class="producttitle">{{ product.commodityName }}</p>
-          <div class="downtitle">
-              <div class="productquantity-container">
-                  <button @click="minus" class="click">-</button>
-                  <input class="productquantity" v-model="quantity" type="number" min="1">
-                  <button @click="plus" class="click">+</button>
-              </div>
-              <p class="productprice">{{ product.price }}</p>
-          </div>
-          <p class="productdis">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore dignissimos sunt similique eaque quo at labore ex tenetur quisquam facere. Veniam dolorem id voluptates quis facere esse expedita nulla est.</p>
-          <button class="addtocartbut">
-              <img src="../assets/cart-24.png" class="carticon">
-              <span @click="addToCart">ADD TO CART</span>
-          </button>
+      <div class="productsection">
+        <img :src="img" class="productimg">
+        <div class="productdetails-container">
+            <p class="producttitle">{{ product.commodityName }}</p>
+            <div class="downtitle">
+                <div class="productquantity-container">
+                    <button @click="minus" class="click">-</button>
+                    <input class="productquantity" v-model="quantity" type="number" min="1">
+                    <button @click="plus" class="click">+</button>
+                </div>
+                <p class="productprice">{{ product.price }}</p>
+            </div>
+            <p class="productdis">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore dignissimos sunt similique eaque quo at labore ex tenetur quisquam facere. Veniam dolorem id voluptates quis facere esse expedita nulla est.</p>
+            <button class="addtocartbut">
+                <img src="../assets/cart-24.png" class="carticon">
+                <span @click="addToCart">ADD TO CART</span>
+            </button>
+        </div>
+        <div class="fullinfocontainer hide">
+            <table>
+                <tr>
+                    <td>finalOdor</td>
+                    <td>{{product.finalOdor}}</td>
+                </tr>
+                <tr>
+                    <td>middleOdor</td>
+                    <td>{{product.middleOdor}}</td>
+                </tr>
+                <tr>
+                    <td>startingOdor</td>
+                    <td>{{product.startingOdor}}</td>
+                </tr>
+                <tr>
+                    <td>gender</td>
+                    <td>{{product.gender}}</td>
+                </tr>
+                <tr>
+                    <td>volume</td>
+                    <td>{{product.volume}}</td>
+                </tr>
+            </table>
+        </div>
+        <img src="../assets/forward.png" class="showFull" @click="toggledetails">
       </div>
-      <div class="fullinfocontainer hide">
-          <table>
-              <tr>
-                  <td>finalOdor</td>
-                  <td>{{product.finalOdor}}</td>
-              </tr>
-              <tr>
-                  <td>middleOdor</td>
-                  <td>{{product.middleOdor}}</td>
-              </tr>
-              <tr>
-                  <td>startingOdor</td>
-                  <td>{{product.startingOdor}}</td>
-              </tr>
-              <tr>
-                  <td>gender</td>
-                  <td>{{product.gender}}</td>
-              </tr>
-              <tr>
-                  <td>volume</td>
-                  <td>{{product.volume}}</td>
-              </tr>
-          </table>
-      </div>
-      <img src="../assets/forward.png" class="showFull" @click="toggledetails">
   </div>
+  <div class="commentsection" v-if="!show404">
+          <p class="commentsection-p">what are custommer say</p>
+          <div class="commentcontainer">
+              <comment v-for="(comment,index) in comments" :key="index" :item="comment"/>
+          </div>
+      </div>
   <div class="notfoundcontainer" v-if="show404">
       <p>404 not found</p>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import comment from '../components/comment.vue';
 
 export default {
     name: 'product',
+    components : {comment},
     data() {
         return {
             product : {},
             quantity : 1,
             show404 : false,
-            img : ''
+            img : '',
+            comments : []
         }
     },
     async beforeMount () {
@@ -72,6 +83,15 @@ export default {
                 if (this.product == undefined) {
                     this.show404 = true
                 }
+            })
+            .catch(err => {
+                console.log(err);
+        })
+        await axios
+            .get('https://localhost:8000/api/v1/comment',{ params: {ID: productID}})
+            .then(res => {
+                this.comments = res.data;
+                console.log(this.comments)
             })
             .catch(err => {
                 console.log(err);
